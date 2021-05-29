c21c95681cf9c15ee110b6ffb49d9f110184b34b
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-05-28 21:58:43 -0400

    read rom path from command line

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        std::cout<<"Not enough arguments! Expected file path!"<<std::endl;
+        std::terminate();
+    }
+
+    std::string romPath(argv[1]);
+    std::cout<<romPath<<std::endl;
+
+    return 0;
+}

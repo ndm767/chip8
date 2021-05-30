2e73a7e103d0a2c23d37dc01404a2a3712b17c1f
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-05-30 11:27:41 -0400

    replaced unsigned int with unsigned short int and then replaced that with typedef usint

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -86,11 +86,11 @@ void Chip8::run(){
 
 void Chip8::processInstructions(uchar byte1, uchar byte2){
 
-    unsigned int bytes[] = {
-        static_cast<unsigned int>((byte1>>4)&0xF),
-        static_cast<unsigned int>(byte1&0xF),
-        static_cast<unsigned int>((byte2>>4)&0xF),
-        static_cast<unsigned int>(byte2 & 0xF)
+    usint bytes[] = {
+        static_cast<usint>((byte1>>4)&0xF),
+        static_cast<usint>(byte1&0xF),
+        static_cast<usint>((byte2>>4)&0xF),
+        static_cast<usint>(byte2 & 0xF)
     };
     switch(bytes[0]){
         case 0x0:
@@ -147,8 +147,8 @@ void Chip8::processInstructions(uchar byte1, uchar byte2){
     }
 }
 
-unsigned int Chip8::c2u(char c){
-    unsigned int t = static_cast<unsigned int>(c);
+usint Chip8::c2u(char c){
+    usint t = static_cast<usint>(c);
     if(c >= 48 && c <= 57)
         return t - 48;
     if( c >= 65 && c <= 70)
@@ -156,7 +156,7 @@ unsigned int Chip8::c2u(char c){
     return t;
 }
 
-bool Chip8::compBytes(unsigned int bytes[4], std::string instr){
+bool Chip8::compBytes(usint bytes[4], std::string instr){
     //compares a set of bytes to the instruction
     if(instr.length() != 4){
         return false;
@@ -175,7 +175,7 @@ bool Chip8::compBytes(unsigned int bytes[4], std::string instr){
     return true;
 }
 
-void Chip8::handle0(unsigned int bytes[4]){
+void Chip8::handle0(usint bytes[4]){
     if(compBytes(bytes, "00E0")){
         std::cout<<"00E0"<<std::endl;
     }else if(compBytes(bytes, "00EE")){
@@ -186,43 +186,43 @@ void Chip8::handle0(unsigned int bytes[4]){
     }
 }
 
-void Chip8::handle1(unsigned int bytes[4]){
+void Chip8::handle1(usint bytes[4]){
     //we already know its 1nnn
     std::cout<<"1nnn"<<std::endl;
 }
 
-void Chip8::handle2(unsigned int bytes[4]){
+void Chip8::handle2(usint bytes[4]){
     //we already know its 2nnn
     std::cout<<"2nnn"<<std::endl;
 }
 
-void Chip8::handle3(unsigned int bytes[4]){
+void Chip8::handle3(usint bytes[4]){
     //we already know its 3xkk
     std::cout<<"3xkk"<<std::endl;
 }
 
-void Chip8::handle4(unsigned int bytes[4]){
+void Chip8::handle4(usint bytes[4]){
     //we already know its 4xkk
     std::cout<<"4xkk"<<std::endl;
 }
 
-void Chip8::handle5(unsigned int bytes[4]){
+void Chip8::handle5(usint bytes[4]){
     if(compBytes(bytes, "5xy0")){
         std::cout<<"5xy0"<<std::endl;
     }
 }
 
-void Chip8::handle6(unsigned int bytes[4]){
+void Chip8::handle6(usint bytes[4]){
     //we already know its 6xkk
     std::cout<<"6xkk"<<std::endl;
 }
 
-void Chip8::handle7(unsigned int bytes[4]){
+void Chip8::handle7(usint bytes[4]){
     //we already know its 7xkk
     std::cout<<"7xkk"<<std::endl;
 }
 
-void Chip8::handle8(unsigned int bytes[4]){
+void Chip8::handle8(usint bytes[4]){
     if(compBytes(bytes, "8xy0")){
         std::cout<<"8xy0"<<std::endl;
     }else if(compBytes(bytes, "8xy1")){
@@ -245,39 +245,39 @@ void Chip8::handle8(unsigned int bytes[4]){
 
 }
 
-void Chip8::handle9(unsigned int bytes[4]){
+void Chip8::handle9(usint bytes[4]){
     if(compBytes(bytes, "9xy0")){
         std::cout<<"9xy0"<<std::endl;
     }
 }
 
-void Chip8::handleA(unsigned int bytes[4]){
+void Chip8::handleA(usint bytes[4]){
     //we already know its Annn
     std::cout<<"Annn"<<std::endl;
 }
 
-void Chip8::handleB(unsigned int bytes[4]){
+void Chip8::handleB(usint bytes[4]){
     //we already know its Bnnn
     std::cout<<"Bnnn"<<std::endl;
 }
 
-void Chip8::handleC(unsigned int bytes[4]){
+void Chip8::handleC(usint bytes[4]){
     //we already know its Cxkk
     std::cout<<"Cxkk"<<std::endl;
 }
 
-void Chip8::handleD(unsigned int bytes[4]){
+void Chip8::handleD(usint bytes[4]){
     //we already know its Dxyn
     std::cout<<"Dxyn"<<std::endl;
 }
 
-void Chip8::handleE(unsigned int bytes[4]){
+void Chip8::handleE(usint bytes[4]){
     if(compBytes(bytes, "ExA1")){
         std::cout<<"ExA1"<<std::endl;
     }
 }
 
-void Chip8::handleF(unsigned int bytes[4]){
+void Chip8::handleF(usint bytes[4]){
     if(compBytes(bytes, "Fx07")){
         std::cout<<"Fx07"<<std::endl;
     }
--- src/chip8.h
+++ src/chip8.h
@@ -3,6 +3,7 @@
 #include <string>
 
 typedef unsigned char uchar;
+typedef unsigned short int usint;
 
 class Chip8{
 public:
@@ -18,35 +19,35 @@ private:
     uchar memory[0xFFF+1];
 
     uchar V[16]; //registers
-    unsigned short int VI; //16 bit register
+    usint VI; //16 bit register
 
-    unsigned short int PC; //program counter
+    usint PC; //program counter
     
     uchar SP; //stack pointer
-    unsigned short int stack[16]; //stack
+    usint stack[16]; //stack
 
     bool running;
 
-    void handle0(unsigned int bytes[4]);
-    void handle1(unsigned int bytes[4]);
-    void handle2(unsigned int bytes[4]);
-    void handle3(unsigned int bytes[4]);
-    void handle4(unsigned int bytes[4]);
-    void handle5(unsigned int bytes[4]);
-    void handle6(unsigned int bytes[4]);
-    void handle7(unsigned int bytes[4]);
-    void handle8(unsigned int bytes[4]);
-    void handle9(unsigned int bytes[4]);
-    void handleA(unsigned int bytes[4]);
-    void handleB(unsigned int bytes[4]);
-    void handleC(unsigned int bytes[4]);
-    void handleD(unsigned int bytes[4]);
-    void handleE(unsigned int bytes[4]);
-    void handleF(unsigned int bytes[4]);
+    void handle0(usint bytes[4]);
+    void handle1(usint bytes[4]);
+    void handle2(usint bytes[4]);
+    void handle3(usint bytes[4]);
+    void handle4(usint bytes[4]);
+    void handle5(usint bytes[4]);
+    void handle6(usint bytes[4]);
+    void handle7(usint bytes[4]);
+    void handle8(usint bytes[4]);
+    void handle9(usint bytes[4]);
+    void handleA(usint bytes[4]);
+    void handleB(usint bytes[4]);
+    void handleC(usint bytes[4]);
+    void handleD(usint bytes[4]);
+    void handleE(usint bytes[4]);
+    void handleF(usint bytes[4]);
 
     //utility functions
-    bool compBytes(unsigned int bytes[4], std::string instr);
-    unsigned int c2u(char c);
+    bool compBytes(usint bytes[4], std::string instr);
+    usint c2u(char c);
 
     //actual opcode instructions
     //prefix 0
@@ -54,10 +55,10 @@ private:
     void ret(); //return from subroutine
     
     //prefix 1
-    void jp(unsigned int addr); //jump to location addr
+    void jp(usint addr); //jump to location addr
 
     //prefix 2
-    void call(unsigned int addr); //call subroutine at addr
+    void call(usint addr); //call subroutine at addr
 
     //prefix 3
     void se3xkk(uchar reg, uchar byte); //skip next instruction if v[reg] == byte

cfbe342e1c61d4dc580c96dd22a567513b05f322
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-05-31 11:22:50 -0400

    added custom scale to emulator

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Based upon [Cowgod's Chip-8 Reference](http://devernay.free.fr/hacks/chip8/C8TEC
 
 ## How to use
 
-`./chip8 [path-to-rom]`  
+`./chip8 [-s scale] [path-to-rom]`  
 A good source of roms is [dmatlack/chip8/roms](https://github.com/dmatlack/chip8/tree/master/roms)  
 
 ### Key layout
--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,19 @@ int main(int argc, char* argv[]){
     }
 
     std::string romPath(argv[1]);
+    int emuscale = 10;
 
-    Chip8 emu(romPath, 10);
+    if(argc == 4){
+        if(strcmp(argv[1], "-s") == 0){
+            emuscale = atoi(argv[2]);
+            romPath = std::string(argv[3]);
+        }else if(strcmp(argv[2], "-s") == 0){
+            emuscale = atoi(argv[3]);
+            romPath = std::string(argv[1]);
+        }
+    }
+
+    Chip8 emu(romPath, emuscale);
     emu.run();
 
     return 0;

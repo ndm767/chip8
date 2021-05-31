0b19658a7b6d0a3ff77724aa9f966253f43463df
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-05-30 22:02:36 -0400

    added key handling

--- src/display.cpp
+++ src/display.cpp
@@ -9,6 +9,10 @@ Display::Display(bool *r){
         }
     }
 
+    for(int i = 0; i<=0xF; i++){
+        keysPressed[i] = false;
+    }
+
     SDL_Init(SDL_INIT_VIDEO);
     gWindow = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 64*scale, 32*scale, SDL_WINDOW_SHOWN);
     gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
@@ -29,7 +33,113 @@ void Display::startFrame(){
         }else if(e.type == SDL_KEYDOWN){
             if(e.key.keysym.scancode == SDL_SCANCODE_ESCAPE){
                 *running = false;
+            }else{
+                switch(e.key.keysym.scancode){
+                case static_cast<int>(Keys::ZERO):
+                    keysPressed[0] = true;
+                    break;
+                case static_cast<int>(Keys::ONE):
+                    keysPressed[1] = true;
+                    break;
+                case static_cast<int>(Keys::TWO):
+                    keysPressed[2] = true;
+                    break;
+                case static_cast<int>(Keys::THREE):
+                    keysPressed[3] = true;
+                    break;
+                case static_cast<int>(Keys::FOUR):
+                    keysPressed[4] = true;
+                    break;
+                case static_cast<int>(Keys::FIVE):
+                    keysPressed[5] = true;
+                    break;
+                case static_cast<int>(Keys::SIX):
+                    keysPressed[6] = true;
+                    break;
+                case static_cast<int>(Keys::SEVEN):
+                    keysPressed[7] = true;
+                    break;
+                case static_cast<int>(Keys::EIGHT):
+                    keysPressed[8] = true;
+                    break;
+                case static_cast<int>(Keys::NINE):
+                    keysPressed[9] = true;
+                    break;
+                case static_cast<int>(Keys::A):
+                    keysPressed[0xA] = true;
+                    break;
+                case static_cast<int>(Keys::B):
+                    keysPressed[0xB] = true;
+                    break;
+                case static_cast<int>(Keys::C):
+                    keysPressed[0xC] = true;
+                    break;
+                case static_cast<int>(Keys::D):
+                    keysPressed[0xD] = true;
+                    break;
+                case static_cast<int>(Keys::E):
+                    keysPressed[0xE] = true;
+                    break;
+                case static_cast<int>(Keys::F):
+                    keysPressed[0xF] = true;
+                    break;
+                default:
+                    break;
+                }
             }
+        }else if(e.type == SDL_KEYUP){
+            switch(e.key.keysym.scancode){
+                case static_cast<int>(Keys::ZERO):
+                    keysPressed[0] = false;
+                    break;
+                case static_cast<int>(Keys::ONE):
+                    keysPressed[1] = false;
+                    break;
+                case static_cast<int>(Keys::TWO):
+                    keysPressed[2] = false;
+                    break;
+                case static_cast<int>(Keys::THREE):
+                    keysPressed[3] = false;
+                    break;
+                case static_cast<int>(Keys::FOUR):
+                    keysPressed[4] = false;
+                    break;
+                case static_cast<int>(Keys::FIVE):
+                    keysPressed[5] = false;
+                    break;
+                case static_cast<int>(Keys::SIX):
+                    keysPressed[6] = false;
+                    break;
+                case static_cast<int>(Keys::SEVEN):
+                    keysPressed[7] = false;
+                    break;
+                case static_cast<int>(Keys::EIGHT):
+                    keysPressed[8] = false;
+                    break;
+                case static_cast<int>(Keys::NINE):
+                    keysPressed[9] = false;
+                    break;
+                case static_cast<int>(Keys::A):
+                    keysPressed[0xA] = false;
+                    break;
+                case static_cast<int>(Keys::B):
+                    keysPressed[0xB] = false;
+                    break;
+                case static_cast<int>(Keys::C):
+                    keysPressed[0xC] = false;
+                    break;
+                case static_cast<int>(Keys::D):
+                    keysPressed[0xD] = false;
+                    break;
+                case static_cast<int>(Keys::E):
+                    keysPressed[0xE] = false;
+                    break;
+                case static_cast<int>(Keys::F):
+                    keysPressed[0xF] = false;
+                    break;
+                default:
+                    break;
+                }
         }
     }
 }
@@ -64,4 +174,8 @@ int Display::xorAtPos(int x, int y, int val){
         return 1;
     }
     return 0;
+}
+
+bool Display::getKeyPress(int key){
+    return keysPressed[key];
 }
--- src/display.h
+++ src/display.h
@@ -2,6 +2,25 @@
 
 #include <SDL2/SDL.h>
 
+enum class Keys{    
+    ZERO = SDL_SCANCODE_0,
+    ONE = SDL_SCANCODE_1,
+    TWO = SDL_SCANCODE_2,
+    THREE = SDL_SCANCODE_3,
+    FOUR = SDL_SCANCODE_4,
+    FIVE = SDL_SCANCODE_5,
+    SIX = SDL_SCANCODE_6,
+    SEVEN = SDL_SCANCODE_7,
+    EIGHT = SDL_SCANCODE_8,
+    NINE = SDL_SCANCODE_9,
+    A = SDL_SCANCODE_A,
+    B = SDL_SCANCODE_B,
+    C = SDL_SCANCODE_C,
+    D = SDL_SCANCODE_D,
+    E = SDL_SCANCODE_E,
+    F = SDL_SCANCODE_F
+};
+
 class Display{
 public:
     Display(bool *r);
@@ -12,11 +31,14 @@ public:
 
     void clear();
     int xorAtPos(int x, int y, int val); //returns 1 if any pixels are erased
+    bool getKeyPress(int key);
 private:
     int pixels[64][32];
     int scale;
 
     bool *running;
+
+    bool keysPressed[0xF+1];
 
     SDL_Window *gWindow;
     SDL_Renderer *gRenderer;
